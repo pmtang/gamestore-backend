3c20cbdd752ceabd72d18d0349b0d659122bc365
Pengming Tang <pengming.tang@fujitsu.com>
2025-03-10 09:00:00 +0100

    add genres endpoints

---
+++ GameStore.Api/Endpoints/GenresEndpoints.cs
@@ -0,0 +1,19 @@
+using System;
+using GameStore.Api.Data;
+using GameStore.Api.Mapping;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameStore.Api.Endpoints;
+
+public static class GenresEndpoints
+{
+    public static RouteGroupBuilder MapGenresEndpoints(this WebApplication app){
+        var group = app.MapGroup("/genres").WithParameterValidation();
+
+        group.MapGet("/", async (GameStoreContext dbContext) => await dbContext.Genres
+                                                                    .Select(genre => genre.ToDto())
+                                                                    .AsNoTracking()
+                                                                    .ToListAsync());
+        return group;
+    }
+}
