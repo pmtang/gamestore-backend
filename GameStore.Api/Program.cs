342852ffab2b917937e1324263ad490c493d7c2f
Pengming <pengmingtang@gmail.com>
2025-03-12 23:32:23 +0100

    add Cors stra

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -6,7 +6,21 @@ var builder = WebApplication.CreateBuilder(args);
 var connString = builder.Configuration.GetConnectionString("GameStore");
 builder.Services.AddSqlite<GameStoreContext>(connString);
 
+//add cors
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()    
+              .AllowAnyMethod()   
+              .AllowAnyHeader();   
+    });
+});
+//add cors
+
 var app = builder.Build();
+
+app.UseCors("AllowAll"); // use cors strategy
 
 app.MapGamesEndpoints();
 app.MapGenresEndpoints();
